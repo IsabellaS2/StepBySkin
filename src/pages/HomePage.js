4cae2fc10219d178c4a5cc4247ca1721257cf54d
IsabellaSulisufi <114592322+IsabellaSulisufi@users.noreply.github.com>
2025-04-25 12:50:21 +0100

    Remove files and setup for development

---
+++ src/pages/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home = () => {
+   return (
+      <div className="home">
+         <h1>StepBySkin</h1>
+         <h3>Welcome! Not sure where to start with your skincare? Tap below to find your routine.</h3>
+         <div className='home-container'>
+            <div className='morning-card'>Morning</div>
+            <div className='evening-card'>Evening</div>
+
+
+         </div>
+      </div>
+   )
+}
+
+export default Home;
